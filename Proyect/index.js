55f06b2b8abbbd0bab7cafcab91d2607a3c3061d
Pizzu43 <fretteleando@live.com>
2023-05-27 18:49:48 -0300

    add apikey

--- Proyect/index.js
+++ Proyect/index.js
@@ -1,22 +1,37 @@
-const carousel = document.querySelector('.carousel');
-const carouselInner = document.querySelector('.carousel-inner');
-const slides = document.querySelectorAll('.slide');
+const apiKey = "xAVejscFZnllSrIt89KWT908e2gkyr0emNFrjGJJ4d2vnUjOUP";
+const apiSecret = "qaZhUXein2fQzgij4btBWUyu8kiBqTqdaO2Ladlg";
 
-const slideWidth = slides[0].clientWidth;
-let currentIndex = 0;
+document.getElementById("obtener-informacion").addEventListener("click", () => {
+  // Obtener token de acceso
+  fetch("https://api.petfinder.com/v2/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${apiSecret}`,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const accessToken = data.access_token;
 
-function slideNext() {
-  currentIndex++;
-  carouselInner.style.transition = 'transform 0.5s ease-in-out';
-  carouselInner.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
-}
-
-carouselInner.addEventListener('transitionend', () => {
-  if (currentIndex >= slides.length - 1) {
-    currentIndex = 0;
-    carouselInner.style.transition = 'none';
-    carouselInner.style.transform = `translateX(0)`;
-  }
+      // Realizar solicitud GET a la API de Petfinder utilizando el token de acceso
+      fetch("https://api.petfinder.com/v2/animals", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Aquí puedes trabajar con los datos devueltos por la API
+          console.log(data);
+        })
+        .catch((error) => {
+          // Manejo de errores
+          console.error(error);
+        });
+    })
+    .catch((error) => {
+      // Manejo de errores
+      console.error(error);
+    });
 });
-
-setInterval(slideNext, 3000);
